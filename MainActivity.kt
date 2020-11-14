8c4550073c1cacd61ced01b18802d43b087fc6d5
K.O <53840638+HTML-CODDER@users.noreply.github.com>
2020-11-14 17:04:11 +0200

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -15,3 +15,70 @@ class MainActivity : AppCompatActivity() {
      ViewModelProviders.of(this).get(QuizViewModel::class.java)
 
 }
+class MainActivity : AppCompatActivity() {
+    ...
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ...
+        val provider: ViewModelProvider = ViewModelProviders.of(this)
+        val quizViewModel = provider.get(QuizViewModel::class.java)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+        ...
+    }
+    ...
+}
+class MainActivity : AppCompatActivity() {
+    ...
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ...
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+        ...
+    }
+    ...
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
+        questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        ...
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ...
+    }
+    private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+
+class MainActivity : AppCompatActivity() {
+    ...
+}
+override fun onPause() {
+    ...
+}
+override fun onSaveInstanceState(savedInstanceState: Bundle) {
+    super.onSaveInstanceState(savedInstanceState)
+    Log.i(TAG, "onSaveInstanceState")
+    savedInstanceState.putInt(KEY_INDEX,  quizViewModel.currentIndex)
+}
+override fun onStop() {
+    ...
+}
+   override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    Log.d(TAG, "onCreate(Bundle?) called")
+    setContentView(R.layout.activity_main)
+    val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+    quizViewModel.currentIndex = currentIndex
+    ...
+}
+    javascript:popUp('/content/images/chap4_9780135245125/elementLinks/activity_lifecycle_alt.png')
+    
