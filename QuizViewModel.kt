4d831311859c783582e1d3163406d2a337309030
K.O <53840638+HTML-CODDER@users.noreply.github.com>
2020-11-14 16:48:00 +0200

    Update QuizViewModel.kt

--- QuizViewModel.kt
+++ QuizViewModel.kt
@@ -8,3 +8,32 @@ class QuizViewModel : ViewModel() {
         Log.d(TAG, "ViewModel instance about to be destroyed")
     }
 }
+    private var currentIndex = 0
+
+    private val questionBank = listOf(
+       Question(R.string.question_australia, true),
+       Question(R.string.question_oceans, true),
+       Question(R.string.question_mideast, false),
+       Question(R.string.question_africa, false),
+       Question(R.string.question_americas, true),
+       Question(R.string.question_asia, true)
+   )
+}
+class QuizViewModel : ViewModel() {
+
+    var currentIndex = 0
+
+    private val questionBank = listOf(
+       ...
+    )
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+
+    }
+}
