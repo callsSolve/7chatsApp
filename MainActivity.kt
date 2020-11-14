b76be9249c402cca7048eb6c80abbd5ade424a46
K.O <53840638+HTML-CODDER@users.noreply.github.com>
2020-11-14 16:37:16 +0200

    ViewModelProvider
    The ViewModelProviders class (note the plural “Providers”) provides instances of the ViewModelProvider class. Your call to ViewModelProviders.of(this) creates and returns a ViewModelProvider associated with the activity.
    ViewModelProvider (no plural), on the other hand, provides instances of ViewModel to the activity. Calling provider.get(QuizViewModel::class.java) returns an instance of QuizViewModel. You will most often see these functions chained together, like so:

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+class MainActivity : AppCompatActivity() {
+    ...
+    override fun onCreate(savedInstanceState: Bundle?) {
+        ...
+        setContentView(R.layout.activity_main)
+
+        val provider: ViewModelProvider = ViewModelProviders.of(this)
+        val quizViewModel = provider.get(QuizViewModel::class.java)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
+        trueButton = findViewById(R.id.true_button)
+        ...
+    }
+    ...
+}
